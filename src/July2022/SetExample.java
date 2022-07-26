cb4873c057aec3915c9a41676aff668f780bd202
Sachin <sachin.shelar@ecotechservices.com>
2022-07-26 23:51:47 +0530

    Added example of Set

---
+++ src/July2022/SetExample.java
@@ -0,0 +1,35 @@
+package July2022;
+/**
+ * Program Of Set
+ * 25-07-2022
+ * Author:Virja Khune
+ */
+
+import java.util.HashSet;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class SetExample {
+
+    public static void main(String[] args){
+
+        Set<Integer> set1 = new HashSet<>();
+
+        // Add elements to the set1
+        set1.add(2);
+        set1.add(3);
+        System.out.println("Set1: " + set1);
+
+
+
+
+        
+    }
+
+}
+
+/**
+ * OUTPUT
+ * Set1: [2, 3]
+ */
